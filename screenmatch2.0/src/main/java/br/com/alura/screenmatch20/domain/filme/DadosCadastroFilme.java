92231191d1f2d92f7701253ab116ef14c79f78c6
Matteus Moreno <40833478+matteusmoreno@users.noreply.github.com>
2023-07-05 16:54:15 -0300

    Add files via upload

---
+++ screenmatch2.0/src/main/java/br/com/alura/screenmatch20/domain/filme/DadosCadastroFilme.java
@@ -0,0 +1,4 @@
+package br.com.alura.screenmatch20.domain.filme;
+
+public record DadosCadastroFilme(String nome, Integer duracao, Integer ano, String genero ) {
+}
---
+++ screenmatch2.0/src/main/java/br/com/alura/screenmatch20/domain/filme/Filme.java
@@ -0,0 +1,62 @@
+package br.com.alura.screenmatch20.domain.filme;
+
+
+import jakarta.persistence.*;
+
+@Table(name = "filmes")
+@Entity
+public class Filme {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Integer duracaoEmMinutos;
+    private Integer anoLancamento;
+    private String genero;
+
+    public Filme() {
+
+    }
+    public Filme(DadosCadastroFilme dados) {
+        this.nome = dados.nome();
+        this.duracaoEmMinutos = dados.duracao();
+        this.anoLancamento = dados.ano();
+        this.genero = dados.genero();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public Integer getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "nome='" + nome + '\'' +
+                ", duracaoEmMinutos=" + duracaoEmMinutos +
+                ", anoLancamento=" + anoLancamento +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+
+    public void atualizaDados(DadosAlteracaoFilme dados) {
+        this.nome = dados.nome();
+        this.duracaoEmMinutos = dados.duracao();
+        this.anoLancamento = dados.ano();
+        this.genero = dados.genero();
+    }
+}
